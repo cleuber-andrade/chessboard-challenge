75308cd2bf713d97106a0d6b9296d3d8bf1c0de2
cleuber-andrade <cleuber_binho@hotmail.com>
2022-01-06 20:58:29 -0300

    update

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "./Linha"
+
+export default function Tabuleiro(){
+  return (
+    <div>
+      <Linha preta/>
+      <Linha />
+      <Linha preta/>
+      <Linha />
+      <Linha preta/>
+      <Linha />
+      <Linha preta/>
+      <Linha />   
+    </div>
+  )
+}
